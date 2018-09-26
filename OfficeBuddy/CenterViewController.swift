1d26c5e2fa4068a6c048a2f2f4722a711c6e918b
changyou <changyou0730@gmail.com>
2018-09-26 18:56:14 +0800

    init

---
+++ OfficeBuddy/CenterViewController.swift
@@ -0,0 +1,33 @@
+
+import UIKit
+
+class CenterViewController: UIViewController {
+  
+  var menuItem: MenuItem! {
+    didSet {
+      title = menuItem.title
+      view.backgroundColor = menuItem.color
+      symbol.text = menuItem.symbol
+    }
+  }
+  
+  @IBOutlet var symbol: UILabel!
+  
+  var menuButton: MenuButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    menuButton = MenuButton()
+    menuButton.tapHandler = {
+      if let containerVC = self.navigationController?.parent as? ContainerViewController {
+        containerVC.toggleSideMenu()
+      }
+    }
+    navigationItem.leftBarButtonItem = UIBarButtonItem(customView: menuButton)
+    menuItem = MenuItem.sharedItems.first!
+  }
+  
+  
+    
+}
